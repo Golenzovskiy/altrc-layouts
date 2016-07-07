6c1b6b03ccd4b93ca4fae4c26f7503a7243b50c4
Sgolenzovskiy <Golenzovskiy@gmail.com>
2016-07-07 16:12:38 +0300

    Clear js

--- js/app.js
+++ js/app.js
@@ -1,55 +1,46 @@
 $(document).ready(function () {
-    $(function () {
-        $("#from").datepicker({
-            dateFormat: "dd.mm.yy",
-            defaultDate: -365,
-            changeYear: true,
-            numberOfMonths: 1,
-            onClose: function (selectedDate) {
-                $("#to").datepicker("option", "minDate", selectedDate);
-            }
-        });
-        $("#to").datepicker({
-            dateFormat: "dd.mm.yy",
-            changeYear: true,
-            numberOfMonths: 1,
-            onClose: function (selectedDate) {
-                $("#from").datepicker("option", "maxDate", selectedDate);
-            }
-        });
-    });
+	$("#from").datepicker({
+		dateFormat: "dd.mm.yy",
+		defaultDate: -365,
+		changeYear: true,
+		numberOfMonths: 1,
+		onClose: function (selectedDate) {
+			$("#to").datepicker("option", "minDate", selectedDate);
+		}
+	});
+	$("#to").datepicker({
+		dateFormat: "dd.mm.yy",
+		changeYear: true,
+		numberOfMonths: 1,
+		onClose: function (selectedDate) {
+			$("#from").datepicker("option", "maxDate", selectedDate);
+		}
+	});
 
-    $(function () {
-        $('#filter').click(function (e) {
+	$('#filter').click(function (e) {
+		e.preventDefault();
+		var l = Ladda.create(this);
+		l.start();
+		$.post("filter.php",
+			{data: $('form').serialize()},
+			function (response) {
+				$('#amount').text(response.amount);
+				$("#searchResult").removeClass('hidden');
+			}, "json")
+			.always(function () {
+				l.stop();
+			});
+		return false;
+	});
 
-            e.preventDefault();
-            var l = Ladda.create(this);
-            l.start();
-            $.post("filter.php",
-                {data: $('form').serialize()},
-                function (response) {
-                    $('#amount').text(response.amount);
-                    $("#searchResult").removeClass('hidden');
-                }, "json")
-                .always(function () {
-                    l.stop();
-                });
-            return false;
-        });
-    });
-
-    $(function () {
-        var sampleTags = ['пищёвка', 'стройка', 'абвгд', 'еёжз'];
-        $('#FieldTags').tagit({
-            availableTags: sampleTags
-        });
-    });
+	var sampleTags = ['пищёвка', 'стройка', 'абвгд', 'еёжз'];
+	$('#FieldTags').tagit({
+		availableTags: sampleTags
+	});
+	
     $('#readOnlyTags').tagit({
         readOnly: true
     });
 	
-	$(function () {
-		$('[data-toggle="tooltip"]').tooltip()
-	});
-
+	$('[data-toggle="tooltip"]').tooltip();
 });
