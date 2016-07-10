0d4f1d9d023a569c9dcbcd1e4610b4128b0ff4c1
Stanislav <golenzovskiy+gitlab@gmail.com>
2016-07-10 15:40:33 +0300

    Issues 10

--- js/app.js
+++ js/app.js
@@ -1,52 +1,51 @@
 $(document).ready(function () {
-    $(function () {
-        $("#from").datepicker({
-            dateFormat: "dd.mm.yy",
-            defaultDate: -365,
-            changeYear: true,
-            numberOfMonths: 1,
-            onClose: function (selectedDate) {
-                $("#to").datepicker("option", "minDate", selectedDate);
-            }
-        });
-        $("#to").datepicker({
-            dateFormat: "dd.mm.yy",
-            changeYear: true,
-            numberOfMonths: 1,
-            onClose: function (selectedDate) {
-                $("#from").datepicker("option", "maxDate", selectedDate);
-            }
-        });
+    $("#from").datepicker({
+        dateFormat: "dd.mm.yy",
+        defaultDate: -365,
+        changeYear: true,
+        numberOfMonths: 1,
+        onClose: function (selectedDate) {
+            $("#to").datepicker("option", "minDate", selectedDate);
+        }
+    });
+    $("#to").datepicker({
+        dateFormat: "dd.mm.yy",
+        changeYear: true,
+        numberOfMonths: 1,
+        onClose: function (selectedDate) {
+            $("#from").datepicker("option", "maxDate", selectedDate);
+        }
     });
 
-    $(function () {
-        $('#filter').click(function (e) {
-
-            e.preventDefault();
-            var l = Ladda.create(this);
-            l.start();
-            $.post("filter.php",
-                {data: $('form').serialize()},
-                function (response) {
-                    console.log(response);
-                    $('#amount').text(response.amount);
-                    $("#searchResult").removeClass('hidden');
-                }, "json")
-                .always(function () {
-                    l.stop();
-                });
-            return false;
-        });
+    $('#filter').click(function (e) {
+        e.preventDefault();
+        var l = Ladda.create(this);
+        l.start();
+        $.post("filter.php",
+            {data: $('form').serialize()},
+            function (response) {
+                $('#amount').text(response.amount);
+                $("#searchResult").removeClass('hidden');
+            }, "json")
+            .always(function () {
+                l.stop();
+            });
+        return false;
     });
 
-    $(function () {
-        var sampleTags = ['пищёвка', 'стройка', 'абвгд', 'еёжз'];
-        $('#FieldTags').tagit({
-            availableTags: sampleTags
-        });
-    });
-    $('#readOnlyTags').tagit({
-        readOnly: true
+    var sampleTags = ['пищёвка', 'стройка', 'абвгд', 'еёжз'];
+    $('#FieldTags').tagit({
+        availableTags: sampleTags
     });
 
+    $('[data-toggle="tooltip"]').tooltip();
+	
+    $(".tag").click(function () {
+        var selectedTag = $(this).data("tag");
+        $("#filterResult").find("tr[data-tags]").each(function() {
+        	if ($.inArray(selectedTag, $(this).data('tags')) == -1) {
+        		$(this).addClass('hidden');
+        	}
+        })
+    })
 });
